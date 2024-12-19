36f41de85226f468bc4e870deeada0e714f05bea
Hwiseong <hw969655@dal.ca>
2024-12-19 00:37:25 -0400

    made a gitignore

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import discord
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
 
 class Client(discord.Client):
     async def on_ready(self):
         print(f'Logged in as {self.user}!')
 
     async def on_message(self, message):
-        print(message)
-        print(f'Message received from {message.author}:{message.content}.')
+        # print(message)
+        print(f'Message received from {message.author}:{message.content}')
         print(f"Message Type: {type(message.content)}")
 
 
@@ -15,4 +19,4 @@ intents = discord.Intents.default()
 intents.messages = True
 intents.message_content = True
 client = Client(intents=intents)
-client.run('MTI5MzM5NDM0MzI5NzY3OTM5MA.GRZDIi.c_aeSMPZsJiPtZo7-r0Yo1zan7GJ1QIu4KMR0U')
+client.run(token)
