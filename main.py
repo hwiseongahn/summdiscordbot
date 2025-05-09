bd6bd6a27e430f2983e4e57ff91c27bf98d705aa
Hwiseong <hw969655@dal.ca>
2025-05-09 17:41:23 -0300

    fixed bug somehow idk how

--- main.py
+++ main.py
@@ -52,8 +52,8 @@ async def bye(interaction: discord.Interaction):
 async def summarize(interaction: discord.Interaction, msg_to_summ: int, send_summary: app_commands.Choice[str]):
     
     send_summary = send_summary.value.lower() == "yes" # convert to boolean
-    await interaction.response.defer(thinking=True)  # defer the response to give time for processing
-    asyncio.sleep()
+    #await interaction.response.defer(thinking=True)  # defer the response to give time for processing
+    #asyncio.sleep()
     print(send_summary)
     
 
